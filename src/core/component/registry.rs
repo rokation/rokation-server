e22e17fa75f0ab275dddafb6239e932e47e1daf1
ej4y <dlwodud821@gmail.com>
2026-07-22 03:02:16 +0900

    feat: add entity despawn

--- src/core/component/registry.rs
+++ src/core/component/registry.rs
@@ -2,6 +2,7 @@ use std::{any::TypeId, collections::HashMap};
 
 use crate::core::{
     component::{component::Component, store::ComponentStore},
+    entity::entity::EntityId,
     storage::storage::Storage,
 };
 
@@ -28,5 +29,11 @@ impl ComponentRegistry {
             .as_any_mut()
             .downcast_mut::<Storage<T>>() // dyn Any안의 타입이 Storage<T>라면 반환
             .unwrap()
+    }
+
+    pub fn remove_entity(&mut self, id: EntityId) {
+        for store in self.stores.values_mut() {
+            store.remove_entity(id);
+        }
     }
 }
--- src/core/component/store.rs
+++ src/core/component/store.rs
@@ -1,19 +1,9 @@
 use std::any::Any;
 
-use crate::core::{component::component::Component, storage::storage::Storage};
+use crate::core::entity::entity::EntityId;
 
 pub trait ComponentStore {
     fn as_any(&self) -> &dyn Any;
     fn as_any_mut(&mut self) -> &mut dyn Any;
-}
-
-// eg. impl Component<Position> ComponentStore for Storage<Position>
-impl<T: Component> ComponentStore for Storage<T> {
-    fn as_any(&self) -> &dyn Any {
-        self
-    }
-
-    fn as_any_mut(&mut self) -> &mut dyn Any {
-        self
-    }
+    fn remove_entity(&mut self, id: EntityId);
 }
