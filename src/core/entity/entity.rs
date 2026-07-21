481fdf0ae0f09d486ac7f31bb41bfd526e8250ba
ej4y <dlwodud821@gmail.com>
2026-07-22 02:31:40 +0900

    refactor: integrate entity, entity id

--- src/core/entity/entity.rs
+++ src/core/entity/entity.rs
@@ -1,5 +1,3 @@
-use crate::core::entity::entity_id::EntityId;
-
 pub struct Entity {
     id: EntityId,
 }
@@ -11,5 +9,31 @@ impl Entity {
 
     pub fn id(&self) -> EntityId {
         self.id
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct EntityId(u64);
+
+impl EntityId {
+    pub fn new(id: u64) -> Self {
+        Self(id)
+    }
+
+    pub fn value(&self) -> u64 {
+        self.0
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct EntityIdGenerator {
+    next: u64,
+}
+
+impl EntityIdGenerator {
+    pub fn next(&mut self) -> EntityId {
+        let id = EntityId::new(self.next);
+        self.next += 1;
+        id
     }
 }
--- src/core/entity/mod.rs
+++ src/core/entity/mod.rs
@@ -1,3 +1,1 @@
 pub mod entity;
-pub mod entity_id;
-pub mod entity_id_generator;
