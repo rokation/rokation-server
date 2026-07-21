62fb9cade37d716095d52fab35b305bdb739d087
ej4y <dlwodud821@gmail.com>
2026-07-22 00:09:57 +0900

    test: add position tests

--- src/core/position.rs
+++ src/core/position.rs
@@ -10,3 +10,25 @@ impl Position {
         Self { x, y, z }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_create_position() {
+        let position = Position::new(10.0, 10.0, 0.0);
+
+        assert_eq!(position.x, 10.0);
+        assert_eq!(position.y, 10.0);
+        assert_eq!(position.z, 0.0);
+    }
+
+    #[test]
+    fn test_compare_position() {
+        let position_a = Position::new(10.0, 10.0, 0.0);
+        let position_b = Position::new(10.0, 10.0, 0.0);
+
+        assert_eq!(position_a, position_b);
+    }
+}
