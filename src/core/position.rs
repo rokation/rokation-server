cd7a4cb912ee6860d336ebe8b5e1f9e364b29c09
ej4y <dlwodud821@gmail.com>
2026-07-22 00:08:14 +0900

    feat: implement position

---
+++ src/core/position.rs
@@ -0,0 +1,12 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Position {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Position {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+}
