8fce056ced4c8a98030b17dc1a74a85f4908762f
ej4y <dlwodud821@gmail.com>
2026-07-22 03:02:30 +0900

    remove: clear previous file

--- src/core/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum CoreError {
-    #[error("{0} not found")]
-    NotFound(String),
-}
-
-pub type Result<T> = std::result::Result<T, CoreError>;
