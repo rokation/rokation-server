c3b1223a481e746588ba322960ae637b9bcf06d9
ej4y <dlwodud821@gmail.com>
2026-07-22 03:02:49 +0900

    feat: add entity remove

--- src/core/storage/storage.rs
+++ src/core/storage/storage.rs
@@ -1,6 +1,9 @@
-use std::collections::HashMap;
+use std::{any::Any, collections::HashMap};
 
-use crate::core::entity::entity::EntityId;
+use crate::core::{
+    component::{component::Component, store::ComponentStore},
+    entity::entity::EntityId,
+};
 
 pub struct Storage<T> {
     data: HashMap<EntityId, T>,
@@ -27,5 +30,24 @@ impl<T> Storage<T> {
 
     pub fn len(&self) -> usize {
         self.data.len()
+    }
+
+    pub fn contains(&self, id: EntityId) -> bool {
+        self.data.contains_key(&id)
+    }
+}
+
+// eg. impl Component<Position> ComponentStore for Storage<Position>
+impl<T: Component> ComponentStore for Storage<T> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    fn remove_entity(&mut self, id: EntityId) {
+        self.data.remove(&id);
     }
 }
