3a458c4517bbf925d6f1a0298445a67059934e51
kimchunchu <dlwodud821@gmail.com>
2026-07-22 12:32:11 +0900

    chore: update module dependency

---
+++ src/core/event/mod.rs
@@ -0,0 +1,3 @@
+pub mod consumer;
+pub mod event;
+pub mod queue;
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,7 @@
 pub mod component;
 pub mod entity;
 pub mod error;
+pub mod event;
 pub mod foundation;
 pub mod storage;
 pub mod world;
