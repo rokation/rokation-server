a3bb8c3a76cc19a8714fc520433c9046f04474d6
kimchunchu <dlwodud821@gmail.com>
2026-07-22 22:34:58 +0900

    feat: add vector from point

--- crates/core/src/geometry/point.rs
+++ crates/core/src/geometry/point.rs
@@ -1,5 +1,21 @@
+use std::ops::Add;
+
+use crate::geometry::vector::Vector3;
+
 pub struct Point3 {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+}
+
+impl Add<Vector3> for Point3 {
+    type Output = Point3;
+
+    fn add(self, rhs: Vector3) -> Self::Output {
+        Point3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
 }
