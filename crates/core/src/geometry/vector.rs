d7189fa52d5b3736e368f5d5c88e1bb572f42144
kimchunchu <dlwodud821@gmail.com>
2026-07-22 22:39:47 +0900

    feat: add vector from vector

--- crates/core/src/geometry/vector.rs
+++ crates/core/src/geometry/vector.rs
@@ -1,5 +1,19 @@
+use std::ops::Add;
+
 pub struct Vector3 {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+}
+
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vector3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
 }
