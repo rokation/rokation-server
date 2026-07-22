78c59b99fb0f6316097038ade4635b5b85aff2df
kimchunchu <dlwodud821@gmail.com>
2026-07-22 23:37:32 +0900

    feat: add bound contains

--- crates/core/src/geometry/bound.rs
+++ crates/core/src/geometry/bound.rs
@@ -4,3 +4,14 @@ pub struct Bound {
     pub min: Point3,
     pub max: Point3,
 }
+
+impl Bound {
+    pub fn contains(&self, point: &Point3) -> bool {
+        point.x >= self.min.x
+            && point.x <= self.max.x
+            && point.y >= self.min.y
+            && point.y <= self.max.y
+            && point.z >= self.min.z
+            && point.z <= self.max.z
+    }
+}
