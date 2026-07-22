3a1df6c107d186beca3719713726b9dc49f2d8f5
kimchunchu <dlwodud821@gmail.com>
2026-07-22 23:47:27 +0900

    feat: add bound intersects

--- crates/core/src/geometry/bound.rs
+++ crates/core/src/geometry/bound.rs
@@ -14,6 +14,15 @@ impl Bound {
             && point.z >= self.min.z
             && point.z <= self.max.z
     }
+
+    pub fn intersects(&self, other: &Bound) -> bool {
+        self.min.x <= other.max.x
+            && self.max.x >= other.min.x
+            && self.min.y <= other.max.y
+            && self.max.y >= other.min.y
+            && self.min.z <= other.max.z
+            && self.max.z >= other.min.z
+    }
 }
 
 #[cfg(test)]
