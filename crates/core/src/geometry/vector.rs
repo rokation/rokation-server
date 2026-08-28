f328d9725675722627c5ecbc704912bf99cf2acb
kimchunchu <dlwodud821@gmail.com>
2026-08-28 19:47:20 +0900

    feat: implement vector, point

--- crates/core/src/geometry/vector.rs
+++ crates/core/src/geometry/vector.rs
@@ -71,10 +71,17 @@ impl Mul<f64> for Vec3 {
     }
 }
 
+#[derive(Debug, Clone, Copy, Default)]
 pub struct Point3 {
     pub x: f64,
     pub y: f64,
     pub z: f64,
+}
+
+impl Point3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
 }
 
 impl Add<Vec3> for Point3 {
