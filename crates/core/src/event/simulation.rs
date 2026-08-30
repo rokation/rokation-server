043ae136caa48d79757baab2e0de40c2472eee08
kimchunchu <dlwodud821@gmail.com>
2026-08-31 02:30:44 +0900

    refactor: remove event

--- crates/core/src/event/simulation.rs
+++ crates/core/src/event/simulation.rs
@@ -6,10 +6,3 @@ use crate::sensor::detection::Detection;
 pub enum SimulationEvent {
     Detection(Detection),
 }
-
-#[derive(Debug, Serialize)]
-pub struct Event {
-    pub tick: u64,
-    pub time: f64,
-    pub kind: SimulationEvent,
-}
