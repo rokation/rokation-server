bee28c6c32ba219a0eb33ecc86b6cdd7cd61a3ab
kimchunchu <dlwodud821@gmail.com>
2026-09-21 16:17:57 +0900

    remove: remove previous files

--- crates/core/src/component/sensor.rs
+++
@@ -1,6 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct Sensor {
-    pub range: f64,
-}
--- crates/core/src/component/transform.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-use crate::geometry::vector::Point3;
-
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct Transform {
-    pub position: Point3,
-}
